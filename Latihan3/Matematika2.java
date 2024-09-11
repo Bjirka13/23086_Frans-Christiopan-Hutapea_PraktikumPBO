2301a34d72e184ea6a5e5c1add4bdcd50f8828b5
Frans Hutapea <147828133+Bjirka13@users.noreply.github.com>
2024-09-11 22:22:59 +0700

    Add files via upload

---
+++ Latihan3/Matematika2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan3;
+
+import Soal1.Matematika;
+
+/**
+ *
+ * @author Family
+ */
+public class Matematika2 extends Matematika{
+    
+    void modulus(){
+    hasil = a % b;
+    System.out.println(a + " " + "Modulus" + " " + b + " " + "=" + " " + hasil);
+    }
+}
