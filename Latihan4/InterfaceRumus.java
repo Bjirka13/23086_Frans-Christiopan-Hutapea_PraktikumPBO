d6e6c45dc3fea8d3de0f2f515130ff97b6b3c2a2
Frans Hutapea <147828133+Bjirka13@users.noreply.github.com>
2024-09-19 20:38:44 +0700

    Add files via upload

---
+++ Latihan4/InterfaceRumus.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Latihan4;
+
+/**
+ *
+ * @author Family
+ */
+public interface InterfaceRumus {
+    public abstract void Balok(int p, int l, int t);
+    public abstract void Kubus(int sisi);
+}
---
+++ Latihan4/RumusBeraksi.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan4;
+
+/**
+ *
+ * @author Family
+ */
+public class RumusBeraksi {
+    public static void main(String[] args){
+    Rumus jawab = new Rumus();
+    
+    jawab.Balok(5,9,3);
+    jawab.Kubus(5);
+    }
+}
